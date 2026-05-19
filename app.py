b149b64ddf60362add7f09a20e7dbe81e5b31e1a
Junior Trejo <dreittrejo@gmail.com>
2026-05-18 22:48:01 -0500

    fix output folder

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
 import pandas as pd
-import psycopg2
 import os
 
-conexion = psycopg2.connect(
-    host="mgg.vps.webdock.cloud",
-    database="dmc",
-    user="usr_ro_dmc_rrhh_estudiantes",
-    password="fZp!jHt0j6%89^B4I*L*29bz4b^",
-    port="5432"
-)
+# Leer CSV
+df = pd.read_csv('ComisionEmpleados_V1_202605.csv')
 
-query = '''
-SELECT empleado_id,
-       nom_empleado,
-       ape_empleado,
-       mnt_salario,
-       mnt_tope_comision
-FROM rrhh.Empleado
-'''
+# Crear carpeta output
+os.makedirs('output', exist_ok=True)
 
-df = pd.read_sql(query, conexion)
+# Ruta del Excel
+ruta = 'output/comisiones.xlsx'
 
-df["comision_calculada"] = df["mnt_salario"] * 0.10
-
-os.makedirs("output", exist_ok=True)
-
-ruta = "output/comisiones.xlsx"
-
+# Generar Excel
 df.to_excel(ruta, index=False)
 
 print("Excel generado correctamente")
-print(df.head())
+print("Archivo:", ruta)
